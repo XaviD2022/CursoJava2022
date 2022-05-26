7052cdf3926ab8ad772cc24b3662a0a7635b3cd9
xavier-domingo <xavier.domingo@allianz.es>
2022-05-26 12:07:15 +0200

    Ejercicio 2

---
+++ src/modulo1/Ejercicio2.java
@@ -0,0 +1,16 @@
+package modulo1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\nNombre\t\tapellido\t\temail\t\t\tteléfono");
+		System.out.println("------\t\t--------\t\t-----\t\t\t--------");
+		System.out.println("\nXavi\t\tDomingo  \t\txdomingo@pepe.com\t666555444");
+		System.out.println("\nAlberto\t\tIglesias\t\taiglesisa@pepe.com\t666555004");
+		System.out.println("\nMaria\t\tGarcia  \t\tmgarcia@pepe.com\t666500444");
+		System.out.println("\nPilar\t\tGomez   \t\tpgomez@pepe.com \t666555999");
+	
+	}
+
+}
