562f8a55685687af1e5e7a5db8fbfd836cf4bb21
xavier-domingo <xavier.domingo@allianz.es>
2022-05-26 10:34:47 +0200

    primera subida

---
+++ src/modulo1/ejemplo.java
@@ -0,0 +1,15 @@
+package modulo1;
+
+public class ejemplo {
+
+	public static void main(String[] args) {
+		//primeras lineas de codigo en java
+		//sysout y cntrl + spaces mostrara las opciones
+		System.out.println("Hola mundo");
+		System.out.println("\n\n\nnombre\t\tapellido\tlink git");
+		System.out.println("\nXavi\t\tDomingo\t\thttps://github.com/XaviD2022/CursoJava2022.git");
+		System.out.println("para las comillas \" ");
+		System.out.println("para las barras\\ ");
+	}
+
+}
