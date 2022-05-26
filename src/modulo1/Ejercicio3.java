707b1acd9606d91cf2c29f8ccab1c3db49fc3a00
xavier-domingo <xavier.domingo@allianz.es>
2022-05-26 12:52:34 +0200

    Ejercicio 3

---
+++ src/modulo1/Ejercicio3.java
@@ -0,0 +1,19 @@
+package modulo1;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\n   significa nueva línea ");
+		System.out.println("\\t   significa un tab de espacio ");
+		System.out.println("\"    es para poner   \" (comillas dobles) dentro del texto por ejemplo \"Belencita\" ");
+		System.out.println("\\    se utiliza para escribir la \\ dentro del texto, por ejemplo \\algo\\ ");
+		System.out.println("\'    se utiliza para las \'(comilla simple) para escribir por ejemplo \'Princesita\' ");
+		
+	}
+
+
+}
+
+
+
